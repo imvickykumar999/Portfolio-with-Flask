5ae6c08d0f1cb88b7b462ac3047a5937729cf262
Vicky Kumar <imvickykumar999@gmail.com>
2020-12-11 03:32:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-# Note we imported request!
+
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup as bs
+# import urllib.request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,6 +25,36 @@ def home():
 @app.route('/skills')
 def skills():
     return render_template('skills.html')
+
+@app.route('/news')
+def news():
+
+    link = 'https://inshorts.com/en/read'
+    req = requests.get(link)
+
+    soup = bs(req.content, 'html5lib')
+    box = soup.findAll('div', attrs = {'class':'news-card z-depth-1'})
+
+    ha,ia,ba,la = [],[],[],[]
+    # for i in range(len(box)):
+    for i in range(6):
+        h = box[i].find('span', attrs = {'itemprop':'headline'}).text
+
+        m = box[i].find('div', attrs = {'class':'news-card-image'})
+        m = m['style'].split("'")[1]
+        # urllib.request.urlretrieve(img, f"static/news/_{i}.jpg")
+
+        b = box[i].find('div', attrs = {'itemprop':'articleBody'}).text
+        try:
+            l = box[i].find('a', attrs = {'class':'source'})['href']
+        except:
+            pass
+
+        ha.append(h)
+        ia.append(m)
+        ba.append(b)
+        la.append(l)
+    return render_template('news.html', ha=ha, ia=ia, ba=ba, la=la)
 
 @app.errorhandler(404)
 def page_not_found(e):
